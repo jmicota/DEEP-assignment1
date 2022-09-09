64368472e0482c012759867be8c26956355257a4
Justyna Micota <justynamicota@gmail.com>
2022-09-09 12:47:25 +0000

    added stock price to training data

--- stock_preprocessing.py
+++ stock_preprocessing.py
@@ -11,10 +11,6 @@ KEYS_1 = 'company'
 KEYS_2 = ['segment', 'year', 'quarter']
 KEYS_3 = ['company', 'year', 'day', 'quarter']
 
-# Column ranges to exclude from data
-EXCLUDE_ID_YEAR_PRICE = (2, -1)
-EXCLUDE_ID_YEAR = (2, 0)
-
 
 def int_to_bool_tuple(num):
     bin_string = format(num, '03b')
@@ -29,14 +25,10 @@ def standardize_data(data):
     return data
 
 
-# Exclude chosen number of first and last columns from data
-def exclude_column_range(data, column_range):
+def exclude_id_and_year_columns(data):
     X = list()
     for _, group in data:
-        if column_range[1] != 0:
-            X.append([list(v[column_range[0]:column_range[1]]) for v in group.values])
-        else:
-            X.append([list(v[column_range[0]:]) for v in group.values])
+            X.append([list(v[2:]) for v in group.values])
     return X
 
 
@@ -59,8 +51,7 @@ def derive_labels_from_stock_prices(data):
     return y
 
 
-# Preprocessing data for network training and testing
-def create_training_dataloaders(info_company, info_quarter, info_daily, info_prices, batch_size, split=80):
+def merge_and_standardize_data(info_company, info_quarter, info_daily, info_prices):
     # Create dataframes
     df_stock_prices = pd.DataFrame(info_prices, columns=LABELS_STOCK_PRICES)
     df_market_analysis = pd.DataFrame(info_quarter, columns=LABELS_MARKET_ANALYSIS)
@@ -68,14 +59,20 @@ def create_training_dataloaders(info_company, info_quarter, info_daily, info_pri
     df_info = pd.DataFrame(info_daily, columns=LABELS_INFO)
 
     # Merge data into the input data frame
-    input_data = pd.merge(left=df_info, right=df_market_segments, left_on=KEYS_1, right_on=KEYS_1)
-    input_data = pd.merge(left=input_data, right=df_market_analysis, left_on=KEYS_2, right_on=KEYS_2)
-    input_data = pd.merge(left=input_data, right=df_stock_prices, left_on=KEYS_3, right_on=KEYS_3)
+    data = pd.merge(left=df_info, right=df_market_segments, left_on=KEYS_1, right_on=KEYS_1)
+    data = pd.merge(left=data, right=df_market_analysis, left_on=KEYS_2, right_on=KEYS_2)
+    data = pd.merge(left=data, right=df_stock_prices, left_on=KEYS_3, right_on=KEYS_3)
 
-    # Fix non-numerical values and group
-    input_data = standardize_data(input_data)
+    # Fix non-numerical values and group data
+    data = standardize_data(data)
+    return data
 
-    X = exclude_column_range(input_data, EXCLUDE_ID_YEAR_PRICE)
+
+# Preprocessing data for network training and testing
+def create_training_dataloaders(info_company, info_quarter, info_daily, info_prices, batch_size, split=80):
+    input_data = merge_and_standardize_data(info_company, info_quarter, info_daily, info_prices)
+
+    X = exclude_id_and_year_columns(input_data)
     y = derive_labels_from_stock_prices(input_data)
     # Shift the labels to "after this information the stock went up" (True/False)
     X = X[:-1]
@@ -98,19 +95,14 @@ def create_training_dataloaders(info_company, info_quarter, info_daily, info_pri
 
 
 # Preprocessing data for Predictor.predict()
-def prepare_prediction_data(info_company, info_quarter, info_daily):
-    # Create dataframes
-    df_market_analysis = pd.DataFrame(info_quarter, columns=LABELS_MARKET_ANALYSIS)
-    df_market_segments = pd.DataFrame(info_company, columns=LABELS_MARKET_SEGMENTS)
-    df_info = pd.DataFrame(info_daily, columns=LABELS_INFO)
+def prepare_prediction_data(info_company, info_quarter, info_daily, current_prices):
+    # Process prices to match expected format
+    info_prices = []
+    for i in range(len(info_company)):
+        info_prices.append((i, info_daily[0][1], info_daily[0][2], info_daily[0][3], current_prices[i]))
 
-    # Merge data into the input data frame
-    input_data = pd.merge(left=df_info, right=df_market_segments, left_on=KEYS_1, right_on=KEYS_1)
-    input_data = pd.merge(left=input_data, right=df_market_analysis, left_on=KEYS_2, right_on=KEYS_2)
+    input_data = merge_and_standardize_data(info_company, info_quarter, info_daily, info_prices)
 
-    # Fix non-numerical values and group
-    input_data = standardize_data(input_data)
-
-    X = exclude_column_range(input_data, EXCLUDE_ID_YEAR)
+    X = exclude_id_and_year_columns(input_data)
     X = torch.tensor(X, dtype=torch.float)
     return X
